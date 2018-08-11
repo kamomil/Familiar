8d2d40bec54afda3f2253023aedf15383b187202
Dafna Hirschfeld <dafna3@gmail.com>
2018-08-11 14:52:18 +0300

    replace links in definitions to full urls

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ var contexts = background.global_contexts;
 function mustache_render(jquey_id, view) {
   var tag = $(jquey_id);
   var rendered = Mustache.to_html(tag.html(), view);
+  rendered = rendered.replace(new RegExp('href="/wiki', 'g'), 'href="https://en.wiktionary.org/wiki');
   console.log(rendered)
   $(tag).html(rendered);
 }
